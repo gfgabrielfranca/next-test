b9406e457b13937ad9e7f004031e2127406659ae
Gabriel França <dev.gabrielfranca@gmail.com>
2021-03-24 10:43:19 -0300

    test

---
+++ pages/profile.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+
+export default function Profile({ time, host }) {
+  return (
+    <>
+      <h1>Profile</h1>
+
+      <p>{time}</p>
+      <p>{host}</p>
+
+      <ul>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          <a href="/about">About (a)</a>
+        </li>
+      </ul>
+    </>
+  )
+}
+
+export async function getServerSideProps() {
+  console.log('server: Profile');
+  
+  const props = { time: (new Date()).toString() };
+
+  return { props };
+}
